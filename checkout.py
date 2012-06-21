928fd170e34f2dd900b049118b41757a27bfe124
sbadame <sbadame@gmail.com>
2012-06-21 09:33:07 -0500

    More ui changes

---
+++ checkout.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt4 import QtGui
+
+from checkoutgui import Ui_MainWindow
+
+class Main(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
