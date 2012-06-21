9a16608228035c2b4e850e104db2432838c0ec5c
sbadame <sbadame@gmail.com>
2012-06-21 09:54:00 -0500

    Starting to plumb in the data

--- checkout.py
+++ checkout.py
@@ -3,12 +3,20 @@ import goodreads
 from checkoutgui import Ui_MainWindow
 from PyQt4 import QtGui
 
+
 class Main(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        available_books = goodreads.listbooks(goodreads.CHECKEDIN_SHELF)
+        for (index, (id, title)) in enumerate(available_books):
+            self.ui.books.insertRow(index)
+            self.ui.books.setItem(index, 0, QtGui.QTableWidgetItem(title))
+            self.ui.books.setItem(index, 2, QtGui.QTableWidgetItem("available"))
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
