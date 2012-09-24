82d6cf1ea5255add744b9e489ac8430fe7f62892
sandro <sbadame@gmail.com>
2012-09-23 20:10:41 -0400

    up to version 5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup( name = "Checkout",
-       version = "0.4",
+       version = "0.5",
        description = "A simple library checkout system that interfaces with goodreads.com",
        options = {"build_exe" : {"includes": "atexit"}},
        executables = [Executable("checkout.py", base=base)])
